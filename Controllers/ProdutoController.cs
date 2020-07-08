b40fcac1dd774c26629a3dcf89a29a1fdd6a81a8
unknown <g.hitoshi.yokogawa@gmail.com>
2020-07-08 10:09:18 -0300

    estrutura de view e controller feitas

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using MVC.Models;
+using MVC.Views;
+
 namespace MVC.Controllers
 {
     public class ProdutoController
     {
-        
+        Produto produtoModel    = new Produto();
+        ProdutoView produtoView = new ProdutoView();
+
+        public void Listar(){
+
+            List<Produto> lista = produtoModel.Ler();
+            produtoView.MostrarNoConsole(lista);
+
+        }
+
+        public void Buscar(string _termo){
+
+            List<Produto> lista = produtoModel.Ler().FindAll(x => x.Preco == float.Parse(_termo));
+            produtoView.MostrarNoConsole(lista);
+
+        }
     }
 }
--- Views/ProdutoView.cs
+++ Views/ProdutoView.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+using System;
+using MVC.Models;
+
 namespace MVC.Views
 {
     public class ProdutoView
     {
-        
+        public void MostrarNoConsole(List<Produto> lista){
+
+            foreach(Produto item in lista)
+            {
+                Console.WriteLine($"{item.Nome} - {item.Preco}");
+            }
+
+        }
     }
 }
